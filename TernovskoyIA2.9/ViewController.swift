b52dbc294f10a063c9c1d9fbbe596d70653eece4
wickapp <wickhomie@gmail.com>
2022-04-16 16:46:14 +0300

    Initial Commit

---
+++ TernovskoyIA2.9/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TernovskoyIA2.9
+//
+//  Created by Илья Терновской on 16.04.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
